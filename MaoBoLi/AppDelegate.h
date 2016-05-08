f92b243bab61471320070d51cadc6480dda8030d
AbelSu131 <abelsu1993@outlook.com>
2016-05-08 11:46:56 +0800

    Initial Commit

---
+++ MaoBoLi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MaoBoLi
+//
+//  Created by AbelSu on 16/5/8.
+//  Copyright © 2016年 AbelSu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MaoBoLi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MaoBoLi
+//
+//  Created by AbelSu on 16/5/8.
+//  Copyright © 2016年 AbelSu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
